69b6d3441994551c77329f0fe48780d3106095df
HusbandosPC\HusbandoMax <husbandomax@gmail.com>
2022-12-27 20:57:23 +1100

    Versions

---
+++ Meshes/Versions.lua
@@ -0,0 +1,21 @@
+local tbl = 
+{
+	[1063] = 
+	{
+		Name = "[HM] - The Keeper of the Lake",
+		Notes = "Inital",
+		Time = 1672134930,
+		Version = 1,
+	},
+	[1069] = 
+	{
+		Name = "[HM] - The Sil'dihn Subterrane",
+		Notes = "Inital",
+		Time = 1672134871,
+		Version = 1,
+	},
+}
+
+
+
+return tbl
