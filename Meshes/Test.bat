ed7c6a7cfd0babb351ade39f90eee9b586a26599
HusbandosPC\HusbandoMax <husbandomax@gmail.com>
2022-12-29 21:56:52 +1100

    Cleanup

--- Meshes/Test.bat
+++
@@ -1,6 +0,0 @@
-cd /D "C:\HusbandoMaxMinion\Meshes\HMMeshesDev\[HM] - The Sil'dihn Subterrane"
-"C:\Program Files\7-Zip\7z.exe" a "C:\HusbandoMaxMinion\Meshes\HMMeshesPacked\[HM] - The Sil'dihn Subterrane.zip" *.mesh
-"C:\Program Files\7-Zip\7z.exe" a "C:\HusbandoMaxMinion\Meshes\HMMeshesPacked\[HM] - The Sil'dihn Subterrane.zip" *.ncs
-"C:\Program Files\7-Zip\7z.exe" a "C:\HusbandoMaxMinion\Meshes\HMMeshesPacked\[HM] - The Sil'dihn Subterrane.zip" *.nav
-"C:\Program Files\7-Zip\7z.exe" a "C:\HusbandoMaxMinion\Meshes\HMMeshesPacked\[HM] - The Sil'dihn Subterrane.zip" *.lua
-pause
