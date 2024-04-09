d6ce7c3334e6ed128daafa2731e2e530adebb81f
HusbandoMax <husbandomax@gmail.com>
2024-04-09 15:39:27 +1000

    Shatter Mesh

--- Meshes/HMMeshesDev/[HM] - The Fields of Glory v2/Version.lua
+++ Meshes/HMMeshesDev/[HM] - The Fields of Glory v2/Version.lua
@@ -1,9 +1,9 @@
 local tbl = 
 {
 	Name = "[HM] - The Fields of Glory v2",
-	Notes = "Release",
-	Time = 1707732189,
-	Version = 2,
+	Notes = "Cleaned Up Mesh + More OMC",
+	Time = 1712638877,
+	Version = 3,
 }
 
 
