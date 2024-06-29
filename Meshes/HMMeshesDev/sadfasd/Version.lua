7cc9f92fe66ab2a589131add380896c9c0052930
HusbandoMax <husbandomax@gmail.com>
2024-06-29 14:43:32 +1000

    Split DB

---
+++ Meshes/HMMeshesDev/sadfasd/Version.lua
@@ -0,0 +1,11 @@
+local tbl = 
+{
+	Name = "sadfasd",
+	Notes = "Inital",
+	Time = 0,
+	Version = 1,
+}
+
+
+
+return tbl
--- ToolboxData/Compress.bat
+++ ToolboxData/Compress.bat
@@ -1,2 +1,3 @@
 cd "C:\HusbandoMaxMinion\ToolboxData\"
 "C:\Program Files\7-Zip\7z.exe" a -tgzip "C:\HusbandoMaxMinion\ToolboxData\HusbandoMaxData.HM.gz" *HM"
+pause
