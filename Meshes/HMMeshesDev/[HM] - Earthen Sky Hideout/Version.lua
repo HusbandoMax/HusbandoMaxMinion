e9c9ac77f5a441a06ce70098608381be4b37e9b1
GrimreaperZ10 <grimz10@gmail.com>
2024-07-02 01:29:20 -0400

    Floor Meshes for Solution 9 and Heritage Found

---
+++ Meshes/HMMeshesDev/[HM] - Earthen Sky Hideout/Version.lua
@@ -0,0 +1,11 @@
+local tbl = 
+{
+	Name = "[HM] - Earthen Sky Hideout",
+	Notes = "Inital",
+	Time = 0,
+	Version = 1,
+}
+
+
+
+return tbl
---
+++ Meshes/HMMeshesDev/[HM] - Heritage Found/Version.lua
@@ -0,0 +1,11 @@
+local tbl = 
+{
+	Name = "[HM] - Heritage Found",
+	Notes = "Inital",
+	Time = 0,
+	Version = 1,
+}
+
+
+
+return tbl
