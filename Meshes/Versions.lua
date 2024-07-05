6e3ef78d5ca13230e4f8196623911758babdeb16
GrimreaperZ10 <grimz10@gmail.com>
2024-07-05 02:39:11 -0400

    Urqopacha Cubes

--- Meshes/Versions.lua
+++ Meshes/Versions.lua
@@ -2,6 +2,7 @@ local tbl =
 {
 	[144] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Gold Saucer",
 		Notes = "Release",
 		Time = 1707541535,
@@ -9,6 +10,7 @@ local tbl =
 	},
 	[158] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Brayflox's Longstop",
 		Notes = "Release",
 		Time = 1672237652,
@@ -16,6 +18,7 @@ local tbl =
 	},
 	[162] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Halatali",
 		Notes = "Release",
 		Time = 1672237910,
@@ -23,6 +26,7 @@ local tbl =
 	},
 	[164] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Tam-Tara Deepcroft",
 		Notes = "Release",
 		Time = 1672237912,
@@ -30,6 +34,7 @@ local tbl =
 	},
 	[168] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Stone Vigil",
 		Notes = "Release",
 		Time = 1672237916,
@@ -37,6 +42,7 @@ local tbl =
 	},
 	[169] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Thousand Maws of Toto-Rak",
 		Notes = "Release",
 		Time = 1672237930,
@@ -44,6 +50,7 @@ local tbl =
 	},
 	[170] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Cutter's Cry",
 		Notes = "Release",
 		Time = 1672237932,
@@ -51,6 +58,7 @@ local tbl =
 	},
 	[172] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Aurum Vale",
 		Notes = "Release",
 		Time = 1672237935,
@@ -58,6 +66,7 @@ local tbl =
 	},
 	[188] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Wanderer's Palace (Hard)",
 		Notes = "Release",
 		Time = 1672237937,
@@ -65,6 +74,7 @@ local tbl =
 	},
 	[402] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Azys Lla",
 		Notes = "Repush 2",
 		Time = 1695562952,
@@ -72,6 +82,7 @@ local tbl =
 	},
 	[441] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Sohm Al",
 		Notes = "Release",
 		Time = 1672237940,
@@ -79,6 +90,7 @@ local tbl =
 	},
 	[442] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Fist of the Father",
 		Notes = "Release",
 		Time = 1712814072,
@@ -86,6 +98,7 @@ local tbl =
 	},
 	[445] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Burden of the Father",
 		Notes = "Release",
 		Time = 1712868201,
@@ -93,6 +106,7 @@ local tbl =
 	},
 	[510] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Pharos Sirius (Hard)",
 		Notes = "Release",
 		Time = 1672237942,
@@ -100,6 +114,7 @@ local tbl =
 	},
 	[534] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Twin Adder Barracks",
 		Notes = "Release",
 		Time = 1672237944,
@@ -107,6 +122,7 @@ local tbl =
 	},
 	[535] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Flame Barracks",
 		Notes = "Release",
 		Time = 1672237946,
@@ -114,6 +130,7 @@ local tbl =
 	},
 	[536] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Maelstrom Barracks",
 		Notes = "Release",
 		Time = 1672237952,
@@ -121,6 +138,7 @@ local tbl =
 	},
 	[554] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Fields of Glory v2",
 		Notes = "Cleaned Up Mesh + More OMC",
 		Time = 1712638877,
@@ -128,6 +146,7 @@ local tbl =
 	},
 	[570] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Palace of the Dead",
 		Notes = "Release",
 		Time = 1717889669,
@@ -135,6 +154,7 @@ local tbl =
 	},
 	[579] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Battlehall",
 		Notes = "Release",
 		Time = 1707551320,
@@ -142,6 +162,7 @@ local tbl =
 	},
 	[613] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Ruby Sea",
 		Notes = "Release",
 		Time = 1693284848,
@@ -149,6 +170,7 @@ local tbl =
 	},
 	[616] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Shisui of the Violet Tides",
 		Notes = "Tweaks",
 		Time = 1717890466,
@@ -156,6 +178,7 @@ local tbl =
 	},
 	[626] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Sirensong Sea",
 		Notes = "Release",
 		Time = 1672237956,
@@ -163,6 +186,7 @@ local tbl =
 	},
 	[693] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Deltascape V3.0",
 		Notes = "Deltascape v3: Maze Total Rework",
 		Time = 1713346990,
@@ -170,6 +194,7 @@ local tbl =
 	},
 	[788] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Saint Mocianne's Arboretum",
 		Notes = "Tweaks",
 		Time = 1717890671,
@@ -177,6 +202,7 @@ local tbl =
 	},
 	[821] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Dohn Mheg",
 		Notes = "Release",
 		Time = 1672237960,
@@ -184,6 +210,7 @@ local tbl =
 	},
 	[823] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Qitana Ravel",
 		Notes = "Release",
 		Time = 1672237962,
@@ -191,6 +218,7 @@ local tbl =
 	},
 	[836] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Malikah's Well",
 		Notes = "Release",
 		Time = 1672237964,
@@ -198,6 +226,7 @@ local tbl =
 	},
 	[837] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Holminster Switch",
 		Notes = "Release",
 		Time = 1672237966,
@@ -205,6 +234,7 @@ local tbl =
 	},
 	[838] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Amaurot",
 		Notes = "Release",
 		Time = 1672237968,
@@ -212,6 +242,7 @@ local tbl =
 	},
 	[884] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Grand Cosmos",
 		Notes = "Release",
 		Time = 1672237970,
@@ -219,6 +250,7 @@ local tbl =
 	},
 	[898] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Anamnesis Anyder",
 		Notes = "Release",
 		Time = 1672237972,
@@ -226,6 +258,7 @@ local tbl =
 	},
 	[916] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Heroes' Gauntlet",
 		Notes = "Release",
 		Time = 1672237975,
@@ -233,6 +266,7 @@ local tbl =
 	},
 	[933] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Matoya's Relict",
 		Notes = "Release",
 		Time = 1672237977,
@@ -240,6 +274,7 @@ local tbl =
 	},
 	[938] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Paglth'an",
 		Notes = "Release",
 		Time = 1672237979,
@@ -247,6 +282,7 @@ local tbl =
 	},
 	[950] = 
 	{
+		Enabled = true,
 		Name = "[HM] - G-Savior Deck",
 		Notes = "Release",
 		Time = 1690639655,
@@ -254,6 +290,7 @@ local tbl =
 	},
 	[952] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Tower of Zot",
 		Notes = "Release",
 		Time = 1672237981,
@@ -261,6 +298,7 @@ local tbl =
 	},
 	[969] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Tower of Babil",
 		Notes = "Release",
 		Time = 1672237983,
@@ -268,6 +306,7 @@ local tbl =
 	},
 	[970] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Vanaspati",
 		Notes = "Release",
 		Time = 1672237986,
@@ -275,6 +314,7 @@ local tbl =
 	},
 	[973] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Dead Ends",
 		Notes = "Release",
 		Time = 1672237989,
@@ -282,6 +322,7 @@ local tbl =
 	},
 	[974] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Ktisis Hyperboreia",
 		Notes = "Release",
 		Time = 1672237991,
@@ -289,6 +330,7 @@ local tbl =
 	},
 	[978] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Aitiascope",
 		Notes = "Release",
 		Time = 1672237994,
@@ -296,6 +338,7 @@ local tbl =
 	},
 	[1036] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Sastasha",
 		Notes = "Release",
 		Time = 1672237997,
@@ -303,6 +346,7 @@ local tbl =
 	},
 	[1037] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Tam-Tara Deepcroft v2",
 		Notes = "Release",
 		Time = 1672238004,
@@ -310,6 +354,7 @@ local tbl =
 	},
 	[1038] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Copperbell Mines v2",
 		Notes = "Release",
 		Time = 1672238007,
@@ -317,6 +362,7 @@ local tbl =
 	},
 	[1039] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Thousand Maws of Toto-Rak v2",
 		Notes = "Release",
 		Time = 1672238023,
@@ -324,6 +370,7 @@ local tbl =
 	},
 	[1040] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Haukke Manor v2",
 		Notes = "Release",
 		Time = 1672238026,
@@ -331,6 +378,7 @@ local tbl =
 	},
 	[1041] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Brayflox's Longstop v2",
 		Notes = "Release",
 		Time = 1672238027,
@@ -338,6 +386,7 @@ local tbl =
 	},
 	[1042] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Stone Vigil v2",
 		Notes = "Release",
 		Time = 1672238029,
@@ -345,6 +394,7 @@ local tbl =
 	},
 	[1043] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Castrum Meridianum v2",
 		Notes = "Release",
 		Time = 1672238033,
@@ -352,6 +402,7 @@ local tbl =
 	},
 	[1044] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Praetorium v2",
 		Notes = "Release",
 		Time = 1672238035,
@@ -359,6 +410,7 @@ local tbl =
 	},
 	[1046] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Navel v2",
 		Notes = "Release",
 		Time = 1672238038,
@@ -366,6 +418,7 @@ local tbl =
 	},
 	[1047] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Howling Eye v2",
 		Notes = "Release",
 		Time = 1672238040,
@@ -373,6 +426,7 @@ local tbl =
 	},
 	[1050] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Alzadaal's Legacy",
 		Notes = "Release",
 		Time = 1672238042,
@@ -380,6 +434,7 @@ local tbl =
 	},
 	[1055] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Unnamed Island",
 		Notes = "Island Path Tweeks",
 		Time = 1699519910,
@@ -387,6 +442,7 @@ local tbl =
 	},
 	[1064] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Sohm Al",
 		Notes = "Inital",
 		Time = 1672207951,
@@ -394,6 +450,7 @@ local tbl =
 	},
 	[1066] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Vault v2",
 		Notes = "Release",
 		Time = 1672238047,
@@ -401,6 +458,7 @@ local tbl =
 	},
 	[1069] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Sil'dihn Subterrane",
 		Notes = "Release",
 		Time = 1672238050,
@@ -408,6 +466,7 @@ local tbl =
 	},
 	[1070] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Fell Court of Troia",
 		Notes = "Release",
 		Time = 1672238053,
@@ -415,6 +474,7 @@ local tbl =
 	},
 	[1097] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Lapis Manalis",
 		Notes = "Release",
 		Time = 1673445700,
@@ -422,6 +482,7 @@ local tbl =
 	},
 	[1109] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Great Gubal Library",
 		Notes = "Release",
 		Time = 1673838894,
@@ -429,6 +490,7 @@ local tbl =
 	},
 	[1110] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Aetherochemical Research Facility",
 		Notes = "Release",
 		Time = 1673838890,
@@ -436,6 +498,7 @@ local tbl =
 	},
 	[1111] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Antitower",
 		Notes = "Release",
 		Time = 1673838785,
@@ -443,6 +506,7 @@ local tbl =
 	},
 	[1112] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Sohr Khai",
 		Notes = "Release",
 		Time = 1673838888,
@@ -450,6 +514,7 @@ local tbl =
 	},
 	[1113] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Xelphatol",
 		Notes = "Release",
 		Time = 1674092415,
@@ -457,6 +522,7 @@ local tbl =
 	},
 	[1114] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Baelsar's Wall",
 		Notes = "Repush",
 		Time = 1675440952,
@@ -464,6 +530,7 @@ local tbl =
 	},
 	[1123] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Shifting Gymnasion Agonon",
 		Notes = "Release",
 		Time = 1673703183,
@@ -471,6 +538,7 @@ local tbl =
 	},
 	[1124] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Eureka Orthos",
 		Notes = "Release",
 		Time = 1703647745,
@@ -478,6 +546,7 @@ local tbl =
 	},
 	[1126] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Aetherfont",
 		Notes = "Release",
 		Time = 1685390968,
@@ -485,6 +554,7 @@ local tbl =
 	},
 	[1137] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Mount Rokkon",
 		Notes = "Fixes",
 		Time = 1690336947,
@@ -492,6 +562,7 @@ local tbl =
 	},
 	[1142] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Sirensong Sea",
 		Notes = "Inital",
 		Time = 1685395508,
@@ -499,6 +570,7 @@ local tbl =
 	},
 	[1143] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Bardam's Mettle",
 		Notes = "Release",
 		Time = 1685738587,
@@ -506,6 +578,7 @@ local tbl =
 	},
 	[1144] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Doma Castle",
 		Notes = "Release",
 		Time = 1685755726,
@@ -513,6 +586,7 @@ local tbl =
 	},
 	[1145] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Castrum Abania",
 		Notes = "Release",
 		Time = 1685765358,
@@ -520,6 +594,7 @@ local tbl =
 	},
 	[1146] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Ala Mhigo",
 		Notes = "Release",
 		Time = 1685780979,
@@ -527,6 +602,7 @@ local tbl =
 	},
 	[1155] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Another Mount Rokkon",
 		Notes = "Release",
 		Time = 1693284857,
@@ -534,6 +610,7 @@ local tbl =
 	},
 	[1164] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Lunar Subterrane",
 		Notes = "Couple stuck fixes.",
 		Time = 1710932213,
@@ -541,6 +618,7 @@ local tbl =
 	},
 	[1167] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Ihuykatumu",
 		Notes = "Release",
 		Time = 1719975225,
@@ -548,6 +626,7 @@ local tbl =
 	},
 	[1172] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Drowned City of Skalla",
 		Notes = "Release",
 		Time = 1699253233,
@@ -555,6 +634,7 @@ local tbl =
 	},
 	[1173] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Burn",
 		Notes = "Release",
 		Time = 1699447441,
@@ -562,6 +642,7 @@ local tbl =
 	},
 	[1174] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Ghimlyt Dark",
 		Notes = "Release",
 		Time = 1699447449,
@@ -569,13 +650,79 @@ local tbl =
 	},
 	[1176] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Aloalo Island",
 		Notes = "Release",
 		Time = 1699253237,
 		Version = 2,
 	},
+	[1185] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Tuliyollal",
+		Notes = "Inital",
+		Time = 1720142912,
+		Version = 1,
+	},
+	[1186] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Solution Nine",
+		Notes = "Inital",
+		Time = 1720142852,
+		Version = 1,
+	},
+	[1187] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Urqopacha",
+		Notes = "Inital",
+		Time = 1720115164,
+		Version = 1,
+	},
+	[1188] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Kozama'uka",
+		Notes = "Inital",
+		Time = 1720110851,
+		Version = 1,
+	},
+	[1189] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Yak T'el",
+		Notes = "Inital",
+		Time = 1720142826,
+		Version = 1,
+	},
+	[1190] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Shaaloani",
+		Notes = "Inital",
+		Time = 1720140066,
+		Version = 1,
+	},
+	[1191] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Heritage Found",
+		Notes = "Inital",
+		Time = 1720139946,
+		Version = 1,
+	},
+	[1192] = 
+	{
+		Enabled = false,
+		Name = "[HM] - Living Memory",
+		Notes = "Inital",
+		Time = 1720142877,
+		Version = 1,
+	},
 	[1193] = 
 	{
+		Enabled = true,
 		Name = "[HM] - Worqor Zormor",
 		Notes = "Release",
 		Time = 1719975229,
@@ -583,6 +730,7 @@ local tbl =
 	},
 	[1194] = 
 	{
+		Enabled = true,
 		Name = "[HM] - The Skydeep Cenote",
 		Notes = "Release",
 		Time = 1719975231,
