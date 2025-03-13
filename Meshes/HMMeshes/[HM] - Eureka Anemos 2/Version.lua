097057b45e12d494be4924a2506bb563221b215a
HusbandoMax <husbandomax@gmail.com>
2025-03-14 10:26:23 +1100

    Eureka Meshes

---
+++ Meshes/HMMeshes/[HM] - Eureka Anemos 2/Version.lua
@@ -0,0 +1,12 @@
+local tbl = 
+{
+	Enabled = false,
+	Name = "[HM] - Eureka Anemos 2",
+	Notes = "Release",
+	Time = 1741908354,
+	Version = 2,
+}
+
+
+
+return tbl
---
+++ Meshes/HMMeshes/[HM] - Eureka Anemos/Version.lua
@@ -0,0 +1,12 @@
+local tbl = 
+{
+	Enabled = false,
+	Name = "[HM] - Eureka Anemos",
+	Notes = "Release",
+	Time = 1741908349,
+	Version = 2,
+}
+
+
+
+return tbl
