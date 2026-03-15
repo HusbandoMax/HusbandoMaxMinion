aa5f40938363b9d7c0fd951f939faf5d0b3d8c05
HusbandoMax <husbandomax@gmail.com>
2026-03-15 21:40:46 +1100

    Mesh Tweaks

--- Meshes/HMMeshesDev/[HM] - Oizys [15]/Version.lua
+++ Meshes/HMMeshesDev/[HM] - Oizys [15]/Version.lua
@@ -3,8 +3,8 @@ local tbl =
 	Enabled = true,
 	Name = "[HM] - Oizys [15]",
 	Notes = "Release",
-	Time = 1771281825,
-	Version = 5,
+	Time = 1773571203,
+	Version = 6,
 }
 
 
--- Meshes/Versions.lua
+++ Meshes/Versions.lua
@@ -1733,8 +1733,8 @@ local tbl =
 		Enabled = true,
 		Name = "[HM] - Oizys [15]",
 		Notes = "Release",
-		Time = 1771281825,
-		Version = 5,
+		Time = 1773571203,
+		Version = 6,
 	},
 	[2601237] = 
 	{
